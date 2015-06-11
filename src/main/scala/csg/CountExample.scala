128c6e9cb1b79c585ff4b8594f7c1c267b131635
cedricbastin <cedricbastin@gmail.com>
2015-06-11 11:07:12 +0200

    count example commented

--- src/main/scala/csg/CountExample.scala
+++ src/main/scala/csg/CountExample.scala
@@ -1,36 +1,46 @@
-package csg
-
-import agParsers._
-
-/**
- * Created by cedricbastin on 02/05/15.
- */
-
-trait CountSig extends AGSig {
-  trait AttrEnv
-  case class Env(max:Int, count:Int, c:Char) extends AttrEnv
-  case object Fail extends AttrEnv
-  def combine(a1:AttrEnv, a2:AttrEnv) = {
-    a1 //useless?
-  }
-  //def next(a:Answer): Answer
-}
-
-trait CountGrammar extends AGParsers with CountSig {
-  //lexical.delimiters ++= List("(", ")", "\\", ".", ":", "=", "->", "{", "}", ",", "*", "+")
-  lexical.reserved ++= List("a", "b", "c")
-
-  def Next: AGParser[AttrEnv] = {
-    rep(lift("a")) ~>> lift("b") ~>> lift("c") ^^ {
-      case a ~ b ~ c => Env(1,2,'c')
-    }
-  }
-}
-
-trait CountAlgebra extends CountSig {
-
-}
-
-class CountExample extends CountGrammar with CountAlgebra {
-
-}
+//package csg
+//
+//import agParsers._
+//
+///**
+// * Created by cedricbastin on 02/05/15.
+// */
+//
+//trait CountSig extends AGSig {
+//  def toAns(list:List[String]):Attr
+//  def validate(list:List[String], env:AttrEnv):Boolean
+//}
+//
+//trait CountGrammar extends AGParsers with CountSig {
+//  //lexical.delimiters ++= List("(", ")", "\\", ".", ":", "=", "->", "{", "}", ",", "*", "+")
+//  lexical.reserved ++= List("a", "b", "c")
+//
+//  def pars = single("a") >>~>> single("b") >>~>> single("c")
+//
+//  def single(s:String): AGParser[Attr] = {
+//    (rep(lift(s)) validate {
+//      case (list, env) => validate(list, env)
+//    }) ^^>> { //validateion already done!
+//      case a => (a.size, )
+//    }
+//  }
+//}
+//
+//trait CountAlgebra extends CountSig {
+//  case class Attr(c:String, count:Int)
+//  type AttrEnv = Int //count
+//
+//  def toAns(list:List[String]) = Attr(list.head, list.size)
+//
+//  def combine(a1:AttrEnv, a2:AttrEnv) = {
+//    a1 //useless but required by abstract definition
+//  }
+//
+//  def validate(list:List[String], env:AttrEnv) = {
+//    (env == 0) || (list.size == env)
+//  }
+//}
+//
+//class CountExample extends CountGrammar with CountAlgebra {
+//
+//}
